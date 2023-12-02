dd6e6fd263580612b88937f6f2d163a5e0df6742
Daniel Rodriguez <Drodriguez661@gmail.com>
2023-12-02 14:45:06 -0600

    added active link highlight

--- js/script.js
+++ js/script.js
@@ -2,19 +2,42 @@ const global = {
     currentPage: window.location.pathname,
 };
 
+//Highlight active link
+function highlightLink() {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach((link) => {
+        if (link.getAttribute('href') === global.currentPage){
+            link.classList.add('active');
+        }
+    });
+}
+
 //Init App
+//Simple router to run code on specific page
 function init() {
     switch (global.currentPage) {
-        case 'flixx-app-theme/':
+        case '/':
+        case '/index.html':
             console.log('Home');
             break;
-        case '/flixx-app-theme/shows.html':
+        case '/shows.html':
             console.log('Shows');
             break;
-        case '/flixx-app-theme/movie-details.html':
+        case '/movie-details.html':
             console.log('Movie Details');
-            break;        
+            break;
+        case '/tv-details.html':
+            console.log('TV Details');
+            break;
+        case '/search.html':
+            console.log('Search');
+            break;
     }
+
+    highlightLink();
 }
 
 document.addEventListener('DOMContentLoaded', init);
+
+an 40501921060
+rn 113193532
