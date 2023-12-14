702f6077d26853032e15efd276a0202af17d622a
Daniel Rodriguez <Drodriguez661@gmail.com>
2023-12-14 15:33:20 -0600

    finished adding displayShowDetails to display on show details page

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ const global = {
     currentPage: window.location.pathname,
 };
 
+
 //Display 20 most popular movies
 async function displayPopularMovies(){
     const { results } = await fetchAPIData('movie/popular');
@@ -68,6 +69,66 @@ async function displayPopularShows(){
         `;
         document.querySelector('#popular-shows').appendChild(div);
     });
+}
+
+// Display Show Details
+async function displayShowDetails() {
+    const showId = window.location.search.split('=') [1];
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    //OverLay for background image
+    displayBackgroundImage('tv', show.backdrop_path)
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `
+    <div class="details-top">
+    <div>
+    ${
+        show.poster_path
+            ? `            <img
+            src="https://image.tmdb.org/t/p/w500${show.poster_path}"
+            class="card-img-top"
+            alt="${show.name}"
+            />`
+                : `            <img
+                src="images/no-image.jpg"
+                class="card-img-top"
+                alt="${show.name}"
+                />`
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Last Air Date: ${show.last_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">number of episodes:</span> ${(show.number_of_episodes)}</li>
+      <li><span class="text-secondary">Last aired:</span> ${show.last_episode_to_air.name}</li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+    ${show.production_companies.map((company) => `<li>${company.name}</li>`).join('')}
+    </div>
+  </div>`;
+
+    document.querySelector('#show-details').appendChild(div);
 }
 
 // Display Movie Details
@@ -209,6 +270,7 @@ function init() {
             displayMovieDetails();
             break;
         case '/tv-details.html':
+            displayShowDetails();
             console.log('TV Details');
             break;
         case '/search.html':
