662a783d36039e55adf562029ffde9b8b5cec7e5
Daniel Rodriguez <Drodriguez661@gmail.com>
2023-12-18 15:06:53 -0600

    finished setting up pagination.

--- js/script.js
+++ js/script.js
@@ -4,7 +4,8 @@ const global = {
         term: '',
         type: '',
         page: 1,
-        totalPages: 1
+        totalPages: 1,
+        totalResults: 0
     },
     api: {
         apiKey: '20d84c69e13a85bb793045d16f7b7d5e',
@@ -202,7 +203,7 @@ async function displayMovieDetails() {
     document.querySelector('#movie-details').appendChild(div);
 }
 
-//Display Backdrop On Details Pages & Styling
+// Display Backdrop On Details Pages & Styling
 function displayBackgroundImage(type, backgroundPath) {
     const overlayDiv = document.createElement('div');
     overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
@@ -234,7 +235,11 @@ async function search() {
     global.search.term = urlParams.get('search-term');
 
     if(global.search.term !== '' && global.search.term !== null) {
-        const { results, total_pages, page } = await searchAPIData();
+        const { results, total_pages, page, total_results } = await searchAPIData();
+
+        global.search.page = page;
+        global.search.totalPages = total_pages;
+        global.search.totalResults = total_results;
 
     if (results.length === 0) {
         showAlert('No results found');
@@ -251,6 +256,11 @@ async function search() {
 }
 
 function displaySearchResults(results) {
+    // Clear previous results
+    document.querySelector('#search-results').innerHTML = '';
+    document.querySelector('#search-results-heading').innerHTML = '';
+    document.querySelector('#pagination').innerHTML = '';
+
     results.forEach(result => {
         const div = document.createElement('div');
         div.classList.add('card');
@@ -277,8 +287,54 @@ function displaySearchResults(results) {
             </p>
         </div>
         `;
+
+        document.querySelector('#search-results-heading').innerHTML = `
+            <h2>${results.length} of ${global.search.totalResults}
+            Results for ${global.search.term}</h2>
+        `;
+
         document.querySelector('#search-results').appendChild(div);
     });
+
+    displayPagination();
+}
+
+// Create and Display Pagination for Search
+function displayPagination() {
+    const div = document.createElement('div');
+    div.classList.add('pagination');
+    div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+    `
+
+    document.querySelector('#pagination').appendChild(div);
+
+    // Disable prev button on first page
+    if (global.search.page === 1 ){
+        document.querySelector('#prev').disabled = true;
+    }
+
+    // Disable next button on last page
+    if (global.search.page === global.search.totalPages ) {
+        document.querySelector('#next').disabled = true;
+    }
+
+    // Next Page 
+    document.querySelector('#next').addEventListener('click', async () => {
+        global.search.page ++;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
+    });
+
+    // Previous Page 
+    document.querySelector('#prev').addEventListener('click', async () => {
+        global.search.page --;
+        const { results, total_pages } = await searchAPIData();
+        displaySearchResults(results);
+    });
+
 }
 
 // Display Slider Movies
@@ -354,7 +410,7 @@ async function searchAPIData(endpoint) {
 
     showSpinner();
 
-    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`)
+    const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`)
 
     const data = await response.json();
 
